abc52a095c5139bdb89b0bc482e88b96ab78642d
Mmy26 <yu.b1994masamura@gmail.com>
2022-02-21 17:11:29 +0900

    エラーメッセージの修正

--- src/views/RegisterAdmin.vue
+++ src/views/RegisterAdmin.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row register-page">
       <form class="col s12" id="reg-form">
-        <div>{{ errorMessage }}</div>
         <div class="row">
+          <div>{{ lastNameError }}</div>
           <div class="input-field col s6">
             <input
               id="last_name"
@@ -14,6 +14,7 @@
             />
             <label for="last_name">姓</label>
           </div>
+          <div>{{ firstNameError }}</div>
           <div class="input-field col s6">
             <input
               id="first_name"
@@ -25,6 +26,7 @@
             <label for="first_name">名</label>
           </div>
         </div>
+        <div>{{ addressError }}</div>
         <div class="row">
           <div class="input-field col s12">
             <input
@@ -37,6 +39,7 @@
             <label for="email">メールアドレス</label>
           </div>
         </div>
+        <div>{{ passwordError }}</div>
         <div class="row">
           <div class="input-field col s12">
             <input
@@ -85,8 +88,16 @@ export default class RegisterAdmin extends Vue {
   private mailAddress = "";
   // パスワード
   private password = "";
-  // エラーメッセージ
-  private errorMessage = "";
+  // 姓のエラーメッセージ
+  private lastNameError = "";
+  // 名のエラーメッセージ
+  private firstNameError = "";
+  // メールアドレスのエラーメッセージ
+  private addressError = "";
+  // パスワードのエラーメッセージ
+  private passwordError = "";
+  // フォームチェッカー
+  private formChecker = true;
 
   /**
    * 管理者情報を登録する.
@@ -104,15 +115,25 @@ export default class RegisterAdmin extends Vue {
     });
     console.dir("response:" + JSON.stringify(response));
 
-    if (
-      this.lastName != "" &&
-      this.firstName != "" &&
-      this.mailAddress != "" &&
-      this.password != ""
-    ) {
+    if (this.lastName === "") {
+      this.firstNameError = "名が入力されていません";
+      this.formChecker = false;
+    }
+    if (this.firstName === "") {
+      this.lastNameError = "姓が入力されていません";
+      this.formChecker = false;
+    }
+    if (this.mailAddress === "") {
+      this.addressError = "メールアドレスが入力されていません";
+      this.formChecker = false;
+    }
+    if (this.password === "") {
+      this.passwordError = "パスワードが入力されていません";
+      this.formChecker = false;
+    }
+
+    if (this.formChecker === true) {
       this.$router.push("/loginAdmin");
-    } else {
-      this.errorMessage = "登録できませんでした";
     }
   }
 }
